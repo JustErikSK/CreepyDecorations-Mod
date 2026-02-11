3246635d01488e2aae1162d9b0e8489ba4cd0ef7
justerik <sebo.erik@gmail.com>
2026-02-11 23:35:48 +0100

    updated icon, updated license, updated README, changed voxel shapes for all blocks

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Add a chilling atmosphere to your world with a mod designed to bring horror-themed decorative blocks to Minecraft!
 
-In order to use this mod, you need Minecraft version 1.20.1 and Forge (47.4.0 is recommended)
+In order to use this mod, you need Minecraft version 1.20.1 and Forge (47.4.0 or above is needed)
 
 ## Current features:
 - **Haunted Pumpkin** block
--- src/main/java/net/withrage/creepydecorations/block/HauntedPumpkin.java
+++ src/main/java/net/withrage/creepydecorations/block/HauntedPumpkin.java
@@ -1,18 +1,55 @@
 package net.withrage.creepydecorations.block;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class HauntedPumpkin extends Block {
+    private static final VoxelShape SHAPE_NORTH = Block.box(0, 0, 0, 16, 16, 16);
     public static final DirectionProperty FACING = DirectionProperty.create("facing", Direction.Plane.HORIZONTAL);
 
     public HauntedPumpkin(Properties properties) {
         super(properties);
         this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext ctx) {
+        Direction dir = state.getValue(FACING);
+        return rotateShape(dir, SHAPE_NORTH);
+    }
+
+    private static VoxelShape rotateShape(Direction direction, VoxelShape shape) {
+        int times = switch (direction) {
+            case SOUTH -> 2;
+            case WEST  -> 1;
+            case EAST  -> 3;
+            default    -> 0;
+        };
+
+        VoxelShape result = shape;
+        for (int i = 0; i < times; i++) {
+            VoxelShape[] buffer = new VoxelShape[]{Shapes.empty()};
+            result.forAllBoxes((minX, minY, minZ, maxX, maxY, maxZ) -> {
+                buffer[0] = Shapes.or(
+                        buffer[0],
+                        Shapes.box(
+                                1.0 - maxZ, minY, minX,
+                                1.0 - minZ, maxY, maxX
+                        )
+                );
+            });
+            result = buffer[0];
+        }
+        return result;
     }
 
     @Override
