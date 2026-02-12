cc51beffc29eee01f2a1764fa781819eb14dec06
justerik <sebo.erik@gmail.com>
2026-02-12 11:01:21 +0100

    reordered some code in the main class

--- src/main/java/net/withrage/creepydecorations/CreepyDecorations.java
+++ src/main/java/net/withrage/creepydecorations/CreepyDecorations.java
@@ -17,45 +17,28 @@ import org.slf4j.Logger;
 
 @Mod(CreepyDecorations.MOD_ID)
 public class CreepyDecorations {
-
     public static final String MOD_ID = "creepydecorations";
-
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public CreepyDecorations(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
-
         modEventBus.addListener(this::commonSetup);
-
         MinecraftForge.EVENT_BUS.register(this);
-
         ModCreativeModeTabs.register(modEventBus);
-
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
-
-        //context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
-
-    }
+    private void commonSetup(final FMLCommonSetupEvent event) {}
 
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-
-    }
+    public void onServerStarting(ServerStartingEvent event) {}
 
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-
-        }
+        public static void onClientSetup(FMLClientSetupEvent event) {}
     }
 }
